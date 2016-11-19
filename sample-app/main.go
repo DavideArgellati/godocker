c8a2220f4f26bedce98d04546725e16876748033
davide <davide.argellati@justdevelop.it>
2016-11-19 18:20:57 +0000

    moved around app added fresh excludes

---
+++ sample-app/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+) 
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+} 
+
+func main() {
+	 
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	http.ListenAndServe(":" + os.Getenv("PORT"), mux)
+}
